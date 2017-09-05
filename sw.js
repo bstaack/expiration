78ef41ed58423cf3f3bf0c612e14e88717f4fb19
branden staack <bsta520@gmail.com>
2017-09-05 10:26:01 -0400

    cache offline

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-/** An empty service worker! */
-self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+// /** An empty service worker! */
+// self.addEventListener('fetch', function(event) {
+//   /** An empty fetch handler! */
+// });
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('the-magic-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon.png',
+        '/site.js',
+        '/style.css',
+        '/sw.js'
+      ]);
+    })
+  );
 });
