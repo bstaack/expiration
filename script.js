7150bee49a861c53f3d5daa3740394f936f00cf7
branden staack <bsta520@gmail.com>
2017-09-05 11:22:01 -0400

    bs

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(function() {
-  navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {console.log('Excellent, registered with scope: ', registration.scope);});
+
+  if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    //Register the ServiceWorker
+    navigator.serviceWorker.register('pwabuider-sw.js', {
+      scope: './'
+    }).then(function(reg) {
+      console.log('Service worker has been registered for scope:'+ reg.scope);
+    });
+  }
 
   var date = new Date(), data = {}, HTMLlist = [], items = [], html = "";
 
