e0b5347d07e99a9e04b3c2a231bbb4de30c069d8
branden staack <bsta520@gmail.com>
2017-11-15 11:29:34 -0500

    init firebase

---
+++ app.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  // Initialize Firebase
+      var config = {
+        apiKey: "AIzaSyAATL4C5TOXS9HsBcZT4d9ICHOQr7ZhpPQ",
+        authDomain: "expiretimer.firebaseapp.com",
+        databaseURL: "https://expiretimer.firebaseio.com",
+        projectId: "expiretimer",
+        storageBucket: "",
+        messagingSenderId: "602109323027"
+      };
+      firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+})
